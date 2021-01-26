3b0c55058d3dfa67876cf05de5a58225b6aaa382
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2021-01-27 00:19:15 +0800

    process actual events in event_handler

--- demo/event_handler/main.cpp
+++ demo/event_handler/main.cpp
@@ -61,9 +61,13 @@ int main(int argc, char** argv)
     register_event_handler(EVENT_TYPE0, event_handler0);
     register_event_handler(EVENT_TYPE1, event_handler1);
 
-    // for (int i = 0; i < max_events; i++) 
-        
+    for (int i = 0; i < max_events; i++) 
+        events[i].type = i % EVENT_TYPE_NUM;
+    
+    for (int i = 0; i < max_events; i++)
+        process_event(&events[i]);
     
 
+    printf("hello world\n");
     return 0;
 }
