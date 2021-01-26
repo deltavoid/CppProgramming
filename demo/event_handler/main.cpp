83bb6a80998a178b0d5f423b053f5687e1be0a59
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2021-01-26 21:41:34 +0800

    update event_handler with register_event_handler

--- demo/event_handler/main.cpp
+++ demo/event_handler/main.cpp
@@ -1,12 +1,44 @@
 #include <cstdio>
 #include <cstdlib>
+#include <cstdint>
+
+
+
+
+enum event_type {
+    EVENT_TYPE0,
+    EVENT_TYPE1,
+    EVENT_TYPE_NUM
+};
+
+struct event {
+    uint8_t type;
+    uint8_t blank;
+    uint16_t blank1;
+    uint32_t blank2;
+};
+
+
+typedef int (*event_handler_t)(struct event* ev);
+
+event_handler_t event_handlers[EVENT_TYPE_NUM];
+
+
+int register_event_handler(enum event_type type, event_handler_t handler)
+{
+    event_handlers[type] = handler;
+}
 
 
 int main(int argc, char** argv)
 {
-    printf("arg:\n");
+    printf("args:");
     for (int i = 0; i < argc; i++)
-        printf("%s\n", argv[i]);
+        printf("%s ", argv[i]);
+    printf("\n");
+
+
+    
 
     return 0;
 }
