34c2716964adf4006087783338da8fa56d3765cc
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2021-01-26 21:34:41 +0800

    update hello

--- hello/main.cpp
+++ hello/main.cpp
@@ -2,20 +2,11 @@
 #include <cstdlib>
 
 
-int main(int argc, char** argv, char** envv)
+int main(int argc, char** argv)
 {
     printf("arg:\n");
     for (int i = 0; i < argc; i++)
         printf("%s\n", argv[i]);
-
-
-    printf("\nenv:\n");
-    char** p = envv;
-    while (*p != NULL)
-    {
-        printf("%s\n", *p);
-        p++;
-    }
 
     return 0;
 }
