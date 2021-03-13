dec9769e5044ac56810c80fdfceac86c4f649eac
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2021-03-13 15:07:18 +0800

    add set_student_name

--- memory/smart_pointer/shared_ptr2.cpp
+++ memory/smart_pointer/shared_ptr2.cpp
@@ -1,11 +1,12 @@
 #include <cstdio>
 #include <cstdlib>
+
+#include <thread>
 #include <memory>
+#include <string>
 
 
 class Student : public std::enable_shared_from_this<Student>
-
-
 {public:
     Student() {  printf("Student::constructor\n");}
     ~Student()  {  printf("Student::destructor\n");}
@@ -26,12 +27,24 @@ void output_count(int cnt, std::string name)
 {
     printf("%s's use_count: %d\n", name.c_str(), cnt);
 }
+
+
+std::shared_ptr<Student> set_student_name(std::shared_ptr<Student> stu, std::string name)
+{
+    stu->setName(name);
+
+    output_count(stu.use_count(), "set_student_name stu");
+
+    return stu;
+}
+
 int main()
 {
     {
         printf("1\n");
         std::shared_ptr<int> ptr(new int(10));
     }
+    printf("\n");
 
     {
         printf("2\n");
@@ -50,6 +63,7 @@ int main()
         output_count(ptr1.use_count(), "ptr1");
         output_count(ptr2.use_count(), "ptr2");
     }
+    printf("\n");
 
     {
         printf("3\n");
@@ -69,6 +83,17 @@ int main()
         output_count(ptr1.use_count(), "ptr1");
         output_count(ptr2.use_count(), "ptr2");
     }
+    printf("\n");
+
+    {
+        std::shared_ptr<Student> stu = std::make_shared<Student>();
+        output_count(stu.use_count(), "stu");
+
+        std::shared_ptr<Student> stu1 = set_student_name(stu, "world");
+        output_count(stu.use_count(), "stu1");
+
+    }
+    printf("\n");
 
 
     return 0;
