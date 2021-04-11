8b8871fa4ce31c3f32c1290fb128435d345ffd34
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2021-04-11 19:32:13 +0800

    update lambda_capture

--- thread/lambda/lambda_capture.cpp
+++ thread/lambda/lambda_capture.cpp
@@ -5,14 +5,75 @@
 
 #include <thread>
 #include <memory>
+#include <functional>
 
 
 
 
 
+class Task {
+public:
+    int _id;
+
+    Task()
+    {
+        printf("Task::Task\n");
+    }
+
+    Task(int id)
+      : _id(id)
+    {
+        printf("Task::Task, id: %d\n", _id);
+    }
+
+    ~Task()
+    {
+        printf("Task::~Task, id: %d\n", _id);
+    }
+
+    Task(const Task& other)
+    {
+        printf("Task::copy constructor\n");
+        this->_id = other._id;
+    }
+
+    Task& operator = (const Task& other)
+    {
+        printf("Task::copy assign operator\n");
+        if  (&other != this)
+        {
+            this->~Task();
+            new (this) Task(other);
+        }
+    }
+
+    Task(Task&& other)
+    {
+        printf("Task::move constructor\n");
+        this->_id = other._id;
+        other._id = 0;
+    }
+
+    Task& operator = (Task&& other)
+    {
+        printf("Task::move assign operator\n");
+        if  (&other != this)
+        {
+            this->~Task();
+            new (this) Task(std::move(other));
+        }
+    }
+
+    void print() const
+    {
+        printf("Task::print, id: %d\n", _id);
+    }
+
+};
 
 
 
+typedef std::function<void(void)> func_t;
 
 
 
@@ -20,6 +81,35 @@
 int main()
 {
     printf("hello world\n");
+
+    printf("1\n");
+    {
+
+        Task task(1);
+
+        [task]() {
+
+            // task._id = 2; // failed , task is const
+            task.print();
+        }();
+        
+    }
+    printf("\n");
+
+
+    printf("2\n");
+    {
+
+        Task task(1);
+
+        [task]() mutable{
+
+            task._id = 2;
+            task.print();
+        }();
+        
+    }
+    printf("\n");
 
 
     return 0;
