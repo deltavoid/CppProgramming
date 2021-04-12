1bae8b7b5f767b4829384248a814e53be5063b6c
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2021-04-12 13:47:46 +0800

    update lambda_return with return type

--- thread/lambda/lambda_return.cpp
+++ thread/lambda/lambda_return.cpp
@@ -48,6 +48,40 @@ int main()
     printf("\n");
 
 
+    printf("3\n");
+    {
+        int a = 1, b = 2;
+
+        int ret = [](int a, int b) {
+
+            if  (a > b)
+                return (long long)a;
+            else
+                return (long long)b;
+
+        }(a, b);
+
+        printf("ret: %d\n", ret);
+    }
+    printf("\n");
+
+
+    printf("4\n");
+    {
+        int a = 1, b = 2;
+
+        long long ret = [](int a, int b) -> long long{
+
+            if  (a > b)
+                return a;
+            else
+                return b;
+
+        }(a, b);
+
+        printf("ret: %lld\n", ret);
+    }
+    printf("\n");
 
     return 0;
 }
