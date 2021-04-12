a4a1be624e9411df7567a31e645904a71e5c78a1
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2021-04-12 13:37:46 +0800

    add labmda_return

---
+++ thread/lambda/lambda_return.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstdint>
+#include <cstring>
+
+
+#include <thread>
+#include <memory>
+#include <functional>
+
+
+
+int main()
+{
+    printf("hello world\n");
+
+    return 0;
+}
