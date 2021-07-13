8c0fd22a59e8fa32472d76320f758c484312d6c4
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2021-07-13 20:24:45 +0800

    update tuple

--- stl/pair_and_tuple/tuple.cpp
+++ stl/pair_and_tuple/tuple.cpp
@@ -4,12 +4,21 @@
 
 #include <memory>
 #include <utility>
+#include <tuple>
+#include <string>
 
 
 
 int main()
 {
     printf("hello world\n");
+
+
+    std::tuple<int, float, std::string> tuple1(1, 1.0, "hello");
+    printf("tuple1.e0: %d, tuple1.e1: %f, tuple1.e2: %s\n",
+            std::get<0>(tuple1), std::get<1>(tuple1), std::get<2>(tuple1).c_str());
+
+
 
     return 0;
 }
