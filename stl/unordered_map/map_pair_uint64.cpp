c308f46755a2f44c04d5626b6e8aa30a4de082c4
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2021-07-13 21:10:06 +0800

    update map_person_int_lambda

--- stl/unordered_map/map_pair_uint64.cpp
+++ stl/unordered_map/map_pair_uint64.cpp
@@ -6,8 +6,16 @@
 #include <memory>
 #include <utility>
 #include <unordered_map>
+#include <functional>
 
 
+
+typedef std::pair<uint64_t, uint64_t> key_type;
+
+struct key_hash {
+
+
+};
 
 
 
@@ -15,5 +23,10 @@
 
 int main()
 {
-    
+    std::unordered_map<std::pair<uint64_t, uint64_t>, uint64_t, 
+            std::function<size_t(const std::pair<uint64_t, uint64_t>)>>
+
+    map(
+
+
 }
