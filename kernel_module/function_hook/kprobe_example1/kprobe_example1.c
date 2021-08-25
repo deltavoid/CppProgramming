ac57d78f734919d6aa88312870ff66914d74d9d2
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2021-08-25 19:02:21 +0800

    update kprobe_example1 with func args

--- kernel_module/function_hook/kprobe_example1/kprobe_example1.c
+++ kernel_module/function_hook/kprobe_example1/kprobe_example1.c
@@ -1,54 +1,20 @@
 
 
-// #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
-
-// #include <linux/init.h>
-// #include <linux/module.h>
-// #include <linux/kernel.h>
-// #include <linux/version.h>
-
-
-// static int __init hello_init(void)
-// {
-//     pr_info("hello inserted\n");
-//     return 0;
-// }
-
-// static void __exit hello_exit(void)
-// {
-//     pr_info("hello removed\n");
-//     pr_debug("-------------------------------------------------\n");
-// }
-
-
-// module_init(hello_init);
-// module_exit(hello_exit);
-// MODULE_LICENSE("GPL");
-
-// SPDX-License-Identifier: GPL-2.0-only
-/*
- * NOTE: This example is works on x86 and powerpc.
- * Here's a sample kernel module showing the use of kprobes to dump a
- * stack trace and selected registers when _do_fork() is called.
- *
- * For more information on theory of operation of kprobes, see
- * Documentation/kprobes.txt
- *
- * You will see the trace data in /var/log/messages and on the console
- * whenever _do_fork() is invoked to create a new process.
- */
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/kprobes.h>
+
 
 #define MAX_SYMBOL_LEN	64
 static char symbol[MAX_SYMBOL_LEN] = "_do_fork";
 module_param_string(symbol, symbol, sizeof(symbol), 0644);
 
 
+
 /* kprobe pre_handler: called just before the probed instruction is executed */
-static int handler_pre(struct kprobe *p, struct pt_regs *regs)
+static int kprobe_symbol_pre_handler(struct kprobe *p, struct pt_regs *regs)
 {
 // #ifdef CONFIG_X86
 //     pr_info("<%s> pre_handler: p->addr = 0x%p, ip = %lx, flags = 0x%lx\n",
@@ -72,16 +38,31 @@ static int handler_pre(struct kprobe *p, struct pt_regs *regs)
 //         p->symbol_name, p->addr, regs->psw.addr, regs->flags);
 // #endif
 
-    unsigned preempt_cnt = preempt_count();
-    if  (preempt_cnt)
-        pr_debug("preempt_count: %x\n", preempt_cnt);
+    // x86_64 function call convention
+    unsigned long args[6] = {
+        regs->di,
+        regs->si,
+        regs->dx,
+        regs->cx,
+        regs->r8,
+        regs->r9,
+    };
+
+    // unsigned preempt_cnt = preempt_count();
+    // if  (preempt_cnt)
+    //     pr_debug("preempt_count: %x\n", preempt_cnt);
+
+    pr_debug("symbol name: %s, symbol addr: %lx, "
+            "arg0: %lu, arg1, %lu, arg2: %lu, arg3: %lu, arg4: %lu, arg5: %lu",
+            p->symbol_name, (unsigned long)p->addr,
+            args[0], args[1], args[2], args[3], args[4], args[5]);
 
     /* A dump_stack() here will give a stack backtrace */
     return 0;
 }
 
 // /* kprobe post_handler: called after the probed instruction is executed */
-// static void handler_post(struct kprobe *p, struct pt_regs *regs,
+// static void kprobe_symbol_post_handler(struct kprobe *p, struct pt_regs *regs,
 //                 unsigned long flags)
 // {
 // #ifdef CONFIG_X86
@@ -111,7 +92,7 @@ static int handler_pre(struct kprobe *p, struct pt_regs *regs)
  * instruction within the pre- or post-handler, or when Kprobes
  * single-steps the probed instruction.
  */
-static int handler_fault(struct kprobe *p, struct pt_regs *regs, int trapnr)
+static int kprobe_generic_fault_handler(struct kprobe *p, struct pt_regs *regs, int trapnr)
 {
     pr_info("fault_handler: p->addr = 0x%p, trap #%dn", p->addr, trapnr);
     /* Return 0 because we don't handle the fault. */
@@ -122,9 +103,9 @@ static int handler_fault(struct kprobe *p, struct pt_regs *regs, int trapnr)
 /* For each probe you need to allocate a kprobe structure */
 static struct kprobe kp = {
     .symbol_name	= symbol,
-    .pre_handler = handler_pre,
-    // .post_handler = handler_post,
-    .fault_handler = handler_fault,
+    .pre_handler = kprobe_symbol_pre_handler,
+    // .post_handler = kprobe_symbol_post_handler,
+    .fault_handler = kprobe_generic_fault_handler,
 };
 
 static int __init kprobe_init(void)
