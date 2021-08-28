27ae1290c366f31c460e3e7e458422d31906a1d1
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2021-08-28 22:39:33 +0800

    add tcp_trace

---
+++ kernel_module/network/tcp_trace/example.c
@@ -0,0 +1,240 @@
+
+
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/version.h>
+
+#include "hook_func_lib.h"
+#include <trace/events/sched.h>
+
+
+
+
+
+// preempt_count display ------------------------------------------
+
+static void preempt_count_display(void)
+{
+    struct task_struct* task_p = current;
+
+    preempt_disable();
+    // comm should use get_task_comm
+    pr_debug("cpu_id: %d, task tid/pid: %d, pid/tgid: %d, comm: %s\n", 
+            smp_processor_id(), task_p->pid, task_p->tgid, task_p->comm);
+    preempt_enable();
+
+    pr_debug("preempt_count: 0x%08x, test_preempt_need_resched: %d\n", preempt_count(), test_preempt_need_resched());
+    // pr_debug("test_preempt_need_resched: %d\n", test_preempt_need_resched());
+}
+
+
+
+
+
+static void trace_local_timer_entry_handler(int id)
+{
+    if  (smp_processor_id() == 0)
+    {
+
+        pr_debug("probe_local_timer_entry, jiffies: %ld\n", jiffies);
+        preempt_count_display();
+
+        dump_stack();
+
+        printk("\n");
+    }
+
+}
+
+
+
+static const char symbol[] = "tcp_v4_syn_recv_sock";
+
+static int kprobe_symbol_pre_handler(struct kprobe *p, struct pt_regs *regs)
+{
+    // x86_64 function call convention
+    unsigned long args[6] = {
+        regs->di,
+        regs->si,
+        regs->dx,
+        regs->cx,
+        regs->r8,
+        regs->r9,
+    };
+
+    pr_debug("kprobe_symbol_pre_handler: symbol name: %s, symbol addr: 0x%lx, "
+            "arg0: %lu, arg1, %lu, arg2: %lu, arg3: %lu, arg4: %lu, arg5: %lu, "
+            "preempt_count: 0x%x\n",
+            p->symbol_name, (unsigned long)p->addr,
+            args[0], args[1], args[2], args[3], args[4], args[5],
+            preempt_count());
+
+    /* A dump_stack() here will give a stack backtrace */
+    // dump_stack();
+
+    return 0;
+}
+
+
+
+static void kprobe_symbol_post_handler(struct kprobe *p, struct pt_regs *regs,
+                unsigned long flags)
+{
+    // x86_64 function call convention
+    unsigned long args[6] = {
+        regs->di,
+        regs->si,
+        regs->dx,
+        regs->cx,
+        regs->r8,
+        regs->r9,
+    };
+
+    pr_debug("kprobe_symbol_post_handler: symbol name: %s, symbol addr: 0x%lx, flags: 0x%lx "
+            "arg0: %lu, arg1, %lu, arg2: %lu, arg3: %lu, arg4: %lu, arg5: %lu, "
+            "preempt_count: 0x%x\n",
+            p->symbol_name, (unsigned long)p->addr, flags,
+            args[0], args[1], args[2], args[3], args[4], args[5],
+            preempt_count());
+
+}
+
+
+
+static int kprobe_generic_fault_handler(struct kprobe *p, struct pt_regs *regs, int trapnr)
+{
+    pr_info("kprobe_generic_fault_handler: symbol name: %s, p->addr = 0x%p, trap #%dn", 
+            p->symbol_name, p->addr, trapnr);
+    /* Return 0 because we don't handle the fault. */
+    return 0;
+}
+
+
+
+struct my_data {
+	ktime_t entry_stamp;
+};
+
+static int kretprobe_symbol_entry_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
+{
+	struct my_data *data;
+
+	// if (!current->mm)
+	// 	return 1;	/* Skip kernel threads */
+
+	data = (struct my_data *)ri->data;
+	data->entry_stamp = ktime_get();
+	return 0;
+}
+
+
+
+static int kretprobe_symbol_ret_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
+{
+	unsigned long retval = regs_return_value(regs);
+	struct my_data *data = (struct my_data *)ri->data;
+	s64 delta;
+	ktime_t now;
+
+	now = ktime_get();
+	delta = ktime_to_ns(ktime_sub(now, data->entry_stamp));
+	pr_info("%s returned %lu and took %lld ns to execute\n",
+			symbol, retval, (long long)delta);
+	return 0;
+}
+
+
+
+// module init -----------------------------------------------------
+
+static struct tracepoint_probe_context sched_probes = {
+    .entries = {
+
+        {
+            .name = "local_timer_entry",
+            .probe = trace_local_timer_entry_handler,
+            .priv = NULL,
+        },
+    },
+    .init_num = 0
+};
+
+
+#define kprobe_num 1
+
+static struct kprobe kprobes[kprobe_num] = {
+
+    {
+        .symbol_name	= symbol,
+        .pre_handler = kprobe_symbol_pre_handler,
+        .post_handler = kprobe_symbol_post_handler,
+        .fault_handler = kprobe_generic_fault_handler,
+    },
+};
+
+
+
+#define kretprobe_num 1
+
+static struct kretprobe kretprobes[kretprobe_num] = {
+    {
+        .kp = {
+            .symbol_name = symbol,
+        },
+	    .handler = kretprobe_symbol_ret_handler,
+	    .entry_handler = kretprobe_symbol_entry_handler,
+	    .data_size = sizeof(struct my_data),
+	    .maxactive = 64,
+    },
+};
+
+
+
+
+static int __init hook_func_example_init(void)
+{
+    pr_debug("hook_func_example_init begin\n");
+
+
+    if  (tracepoint_probe_context_find_tracepoints(&sched_probes) < 0)
+    {   pr_warn("find tracepoints failed\n");
+        return -1;
+    }
+
+    if  (tracepoint_probe_context_register_probes(&sched_probes) < 0)
+    {   pr_warn("register probes failed\n");
+        return -1;
+    }
+
+    if  (kprobes_init(kprobes, kprobe_num) < 0)
+        return -1;
+
+    
+    if  (kretprobes_init(kretprobes, kretprobe_num) < 0)
+        return -1;
+
+    pr_debug("hook_func_example_init end\n");
+    return 0;
+}
+
+static void __exit hook_func_example_exit(void)
+{
+    pr_debug("hook_func_example_exit begin\n");
+
+    tracepoint_probe_context_unregister_probes(&sched_probes);
+
+    kprobes_exit(kprobes, kprobe_num);
+
+    kretprobes_exit(kretprobes, kretprobe_num);
+    
+
+    pr_debug("hook_func_example_exit end\n");
+    pr_debug("------------------------------------------------------------------\n");
+}
+
+module_init(hook_func_example_init)
+module_exit(hook_func_example_exit)
+MODULE_LICENSE("GPL");
---
+++ kernel_module/network/tcp_trace/hook_func_lib.c
@@ -0,0 +1,142 @@
+#include "hook_func_lib.h"
+
+
+
+
+
+// tracepoint_probe_context ----------------------------------------
+
+static void tracepoint_lookup(struct tracepoint *tp, void *priv)
+{
+    int i;
+    struct tracepoint_probe_context* ctx = priv;
+
+    if  (ctx->found_num == ctx->init_num)
+        return;
+
+    for (i = 0; i < ctx->init_num; i++)
+    {   struct tracepoint_probe_entry* entry = &ctx->entries[i];
+
+        if  (entry->tp || strcmp(tp->name, entry->name))
+            continue;
+        entry->tp = tp;
+        ctx->found_num++;
+    }
+}
+
+int tracepoint_probe_context_find_tracepoints(struct tracepoint_probe_context* ctx)
+{
+    int i;
+    for (i = 0; i < ctx->init_num; i++)
+        ctx->entries[i].tp = NULL;
+    ctx->found_num = 0;
+
+
+    for_each_kernel_tracepoint(tracepoint_lookup, ctx);
+
+    if  (ctx->found_num != ctx->init_num)
+        return -1;
+    return 0;
+}
+
+int tracepoint_probe_context_register_probes(struct tracepoint_probe_context* ctx)
+{
+    int i;
+
+    for (i = 0; i < ctx->init_num; i++)
+    {   struct tracepoint_probe_entry* entry = &ctx->entries[i];
+        int ret = tracepoint_probe_register(entry->tp, entry->probe, entry->priv);
+        if  (ret)
+            pr_warn("trace_%s probe failed\n", entry->name);
+
+    }
+
+    return 0;
+}
+
+void tracepoint_probe_context_unregister_probes(struct tracepoint_probe_context* ctx)
+{
+    int i;
+
+    for (i = 0; i < ctx->init_num; i++)
+    {   struct tracepoint_probe_entry* entry = &ctx->entries[i];
+        tracepoint_probe_unregister(entry->tp, entry->probe, entry->priv);
+    }
+
+    tracepoint_synchronize_unregister();
+}
+
+
+
+
+
+
+// kprobes_init --------------------------------------------------------------------------------
+
+int kprobes_init(struct kprobe* kps, int num)
+{
+    int i, j;
+    int ret;
+
+    for (i = 0; i < num; i++)
+    {
+        ret = register_kprobe(&kps[i]);
+        if  (ret < 0)
+        {   pr_warn("kprobes_init: register_kprobe failed, i: %d, symbol name: %s\n",
+                    i, kps[i].symbol_name);
+            goto kprobes_init_failed;
+        }
+    }
+
+    return 0;
+
+kprobes_init_failed:
+    for (j = i - 1; j >= 0; j--)
+        unregister_kprobe(&kps[j]);
+
+    return ret;
+}
+
+void kprobes_exit(struct kprobe* kps, int num)
+{
+    int i;
+
+    for (i = num - 1; i >= 0; i--)
+        unregister_kprobe(&kps[i]);
+}
+
+
+
+// kretprobes_init --------------------------------------------------------------------------------
+
+int kretprobes_init(struct kretprobe* kps, int num)
+{
+    int i, j;
+    int ret;
+
+    for (i = 0; i < num; i++)
+    {
+        ret = register_kretprobe(&kps[i]);
+        if  (ret < 0)
+        {   pr_warn("kprobes_init: register_kprobe failed, i: %d, symbol name: %s\n",
+                    i, kps[i].kp.symbol_name);
+            goto kretprobes_init_failed;
+        }
+    }
+
+    return 0;
+
+kretprobes_init_failed:
+    for (j = i - 1; j >= 0; j--)
+        unregister_kretprobe(&kps[j]);
+
+    return ret;
+}
+
+void kretprobes_exit(struct kretprobe* kps, int num)
+{
+    int i;
+
+    for (i = num - 1; i >= 0; i--)
+        unregister_kretprobe(&kps[i]);
+}
