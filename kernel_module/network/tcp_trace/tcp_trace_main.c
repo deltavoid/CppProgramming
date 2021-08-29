f57e778c1950879c1dd517353ec46e51c6e1ab29
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2021-08-30 01:37:48 +0800

    update tcp_v4_syn_recv_sock

--- kernel_module/network/tcp_trace/tcp_trace_main.c
+++ kernel_module/network/tcp_trace/tcp_trace_main.c
@@ -51,9 +51,9 @@ static void trace_local_timer_entry_handler(int id)
 
 
 
-static const char symbol[] = "tcp_v4_syn_recv_sock";
+// static const char symbol[] = "tcp_v4_syn_recv_sock";
 
-static int kprobe_symbol_pre_handler(struct kprobe *p, struct pt_regs *regs)
+static int kprobe_tcp_v4_syn_recv_sock_pre_handler(struct kprobe *p, struct pt_regs *regs)
 {
     // x86_64 function call convention
     unsigned long args[6] = {
@@ -65,7 +65,7 @@ static int kprobe_symbol_pre_handler(struct kprobe *p, struct pt_regs *regs)
         regs->r9,
     };
 
-    pr_debug("kprobe_symbol_pre_handler: symbol name: %s, symbol addr: 0x%lx, "
+    pr_debug("kprobe_tcp_v4_syn_recv_sock_pre_handler: symbol name: %s, symbol addr: 0x%lx, "
             "arg0: %lu, arg1, %lu, arg2: %lu, arg3: %lu, arg4: %lu, arg5: %lu, "
             "preempt_count: 0x%x\n",
             p->symbol_name, (unsigned long)p->addr,
@@ -80,71 +80,71 @@ static int kprobe_symbol_pre_handler(struct kprobe *p, struct pt_regs *regs)
 
 
 
-static void kprobe_symbol_post_handler(struct kprobe *p, struct pt_regs *regs,
-                unsigned long flags)
-{
-    // x86_64 function call convention
-    unsigned long args[6] = {
-        regs->di,
-        regs->si,
-        regs->dx,
-        regs->cx,
-        regs->r8,
-        regs->r9,
-    };
+// static void kprobe_symbol_post_handler(struct kprobe *p, struct pt_regs *regs,
+//                 unsigned long flags)
+// {
+//     // x86_64 function call convention
+//     unsigned long args[6] = {
+//         regs->di,
+//         regs->si,
+//         regs->dx,
+//         regs->cx,
+//         regs->r8,
+//         regs->r9,
+//     };
 
-    pr_debug("kprobe_symbol_post_handler: symbol name: %s, symbol addr: 0x%lx, flags: 0x%lx "
-            "arg0: %lu, arg1, %lu, arg2: %lu, arg3: %lu, arg4: %lu, arg5: %lu, "
-            "preempt_count: 0x%x\n",
-            p->symbol_name, (unsigned long)p->addr, flags,
-            args[0], args[1], args[2], args[3], args[4], args[5],
-            preempt_count());
+//     pr_debug("kprobe_symbol_post_handler: symbol name: %s, symbol addr: 0x%lx, flags: 0x%lx "
+//             "arg0: %lu, arg1, %lu, arg2: %lu, arg3: %lu, arg4: %lu, arg5: %lu, "
+//             "preempt_count: 0x%x\n",
+//             p->symbol_name, (unsigned long)p->addr, flags,
+//             args[0], args[1], args[2], args[3], args[4], args[5],
+//             preempt_count());
 
-}
-
-
-
-static int kprobe_generic_fault_handler(struct kprobe *p, struct pt_regs *regs, int trapnr)
-{
-    pr_info("kprobe_generic_fault_handler: symbol name: %s, p->addr = 0x%p, trap #%dn", 
-            p->symbol_name, p->addr, trapnr);
-    /* Return 0 because we don't handle the fault. */
-    return 0;
-}
+// }
 
 
 
-struct my_data {
-	ktime_t entry_stamp;
-};
-
-static int kretprobe_symbol_entry_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
-{
-	struct my_data *data;
-
-	// if (!current->mm)
-	// 	return 1;	/* Skip kernel threads */
-
-	data = (struct my_data *)ri->data;
-	data->entry_stamp = ktime_get();
-	return 0;
-}
+// static int kprobe_generic_fault_handler(struct kprobe *p, struct pt_regs *regs, int trapnr)
+// {
+//     pr_info("kprobe_generic_fault_handler: symbol name: %s, p->addr = 0x%p, trap #%dn", 
+//             p->symbol_name, p->addr, trapnr);
+//     /* Return 0 because we don't handle the fault. */
+//     return 0;
+// }
 
 
 
-static int kretprobe_symbol_ret_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
-{
-	unsigned long retval = regs_return_value(regs);
-	struct my_data *data = (struct my_data *)ri->data;
-	s64 delta;
-	ktime_t now;
+// struct my_data {
+// 	ktime_t entry_stamp;
+// };
 
-	now = ktime_get();
-	delta = ktime_to_ns(ktime_sub(now, data->entry_stamp));
-	pr_info("%s returned %lu and took %lld ns to execute\n",
-			symbol, retval, (long long)delta);
-	return 0;
-}
+// static int kretprobe_symbol_entry_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
+// {
+// 	struct my_data *data;
+
+// 	// if (!current->mm)
+// 	// 	return 1;	/* Skip kernel threads */
+
+// 	data = (struct my_data *)ri->data;
+// 	data->entry_stamp = ktime_get();
+// 	return 0;
+// }
+
+
+
+// static int kretprobe_symbol_ret_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
+// {
+// 	unsigned long retval = regs_return_value(regs);
+// 	struct my_data *data = (struct my_data *)ri->data;
+// 	s64 delta;
+// 	ktime_t now;
+
+// 	now = ktime_get();
+// 	delta = ktime_to_ns(ktime_sub(now, data->entry_stamp));
+// 	pr_info("%s returned %lu and took %lld ns to execute\n",
+// 			symbol, retval, (long long)delta);
+// 	return 0;
+// }
 
 
 
@@ -166,8 +166,8 @@ static struct tracepoint_probe_context sched_probes = {
 
 static struct kprobe kprobes[kprobe_num] = {
     {
-        .symbol_name	= symbol,
-        .pre_handler = kprobe_symbol_pre_handler,
+        .symbol_name	= "tcp_v4_syn_recv_sock",
+        .pre_handler = kprobe_tcp_v4_syn_recv_sock_pre_handler,
         // .post_handler = kprobe_symbol_post_handler,
         // .fault_handler = kprobe_generic_fault_handler,
     },
@@ -175,18 +175,18 @@ static struct kprobe kprobes[kprobe_num] = {
 
 
 
-#define kretprobe_num 1
+#define kretprobe_num 0
 
 static struct kretprobe kretprobes[kretprobe_num] = {
-    {
-        .kp = {
-            .symbol_name = symbol,
-        },
-	    .handler = kretprobe_symbol_ret_handler,
-	    // .entry_handler = kretprobe_symbol_entry_handler,
-	    // .data_size = sizeof(struct my_data),
-	    .maxactive = 64,
-    },
+    // {
+    //     .kp = {
+    //         .symbol_name = symbol,
+    //     },
+	//     .handler = kretprobe_symbol_ret_handler,
+	//     // .entry_handler = kretprobe_symbol_entry_handler,
+	//     // .data_size = sizeof(struct my_data),
+	//     .maxactive = 64,
+    // },
 };
 
 
