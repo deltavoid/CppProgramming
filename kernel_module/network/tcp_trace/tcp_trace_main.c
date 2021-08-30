73f13bdf541b3b45abcab055dc3ea5549fef9727
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2021-08-30 21:48:43 +0800

    add inet_csk_destroy_sock

--- kernel_module/network/tcp_trace/tcp_trace_main.c
+++ kernel_module/network/tcp_trace/tcp_trace_main.c
@@ -426,6 +426,19 @@ static int kprobe__tcp_done__pre_handler(struct kprobe *p, struct pt_regs *regs)
 }
 
 
+static int kprobe__inet_csk_destroy_sock__pre_handler(struct kprobe *p, struct pt_regs *regs)
+{
+    struct sock* sk = (struct sock*)x86_64_get_regs_arg(regs, 0);
+
+    if  (!sock_filter_and_display(sk, "kprobe:inet_csk_destroy_sock: "))
+        return 0;
+
+    pr_debug("\n");
+    return 0;
+}
+
+
+
 // module init -----------------------------------------------------
 
 static struct tracepoint_probe_context sched_probes = {
@@ -440,7 +453,7 @@ static struct tracepoint_probe_context sched_probes = {
 };
 
 
-#define kprobe_num 14
+#define kprobe_num 15
 
 static struct kprobe kprobes[kprobe_num] = {
     {
@@ -498,6 +511,10 @@ static struct kprobe kprobes[kprobe_num] = {
     {
         .symbol_name	= "tcp_done",
         .pre_handler = kprobe__tcp_done__pre_handler,
+    },
+    {
+        .symbol_name	= "inet_csk_destroy_sock",
+        .pre_handler = kprobe__inet_csk_destroy_sock__pre_handler,
     },
 };
 
