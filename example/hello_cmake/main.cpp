b3a383999d8548ec3b29e80a383dd2dfd59a7f7c
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2021-09-28 21:44:28 +0800

    add env in hello_cmake

--- example/hello_cmake/main.cpp
+++ example/hello_cmake/main.cpp
@@ -2,8 +2,13 @@
 #include <cstdlib>
 
 
-int main(int argc, char** argv)
+int main(int argc, char** argv, char** env)
 {
+    printf("env:\n");
+    for (int i = 0; env[i] != NULL; i++)
+        printf("%s\n", env[i]);
+    printf("\n");
+
     printf("arg:\n");
     for (int i = 0; i < argc; i++)
         printf("%s\n", argv[i]);
