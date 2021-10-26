2a3b5c46246ec949231e1c3563e7d84132d4ebc4
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2021-10-26 21:29:44 +0800

    add tcp_keepalive_timer

--- kernel_module/network/tcp_trace/tcp_trace_main.c
+++ kernel_module/network/tcp_trace/tcp_trace_main.c
@@ -639,6 +639,17 @@ static int kprobe__tcp_delack_timer__pre_handler(struct kprobe *p, struct pt_reg
     return 0;
 }
 
+static int kprobe__tcp_keepalive_timer__pre_handler(struct kprobe *p, struct pt_regs *regs)
+{
+    struct sock* sk = (struct sock*)x86_64_get_regs_arg(regs, 0);
+
+    if  (!sock_filter_and_display(sk, 3, "kprobe:tcp_keepalive_timer: "))
+        return 0;
+
+    pr_debug("\n");
+    return 0;
+}
+
 
 
 // module init -----------------------------------------------------
@@ -660,7 +671,7 @@ static struct tracepoint_probe_context sched_probes = {
 };
 
 
-#define kprobe_num 28
+#define kprobe_num 29
 
 static struct kprobe kprobes[kprobe_num] = {
     {
@@ -778,6 +789,10 @@ static struct kprobe kprobes[kprobe_num] = {
     {
         .symbol_name	= "tcp_delack_timer",
         .pre_handler = kprobe__tcp_delack_timer__pre_handler,
+    },
+    {
+        .symbol_name	= "tcp_keepalive_timer",
+        .pre_handler = kprobe__tcp_keepalive_timer__pre_handler,
     },
 };
 
