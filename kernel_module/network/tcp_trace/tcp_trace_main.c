e01771b5c949dd7b98e3438ec3fcc2e0f825397f
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2021-10-26 20:55:48 +0800

    add __tcp_transmit_skb

--- kernel_module/network/tcp_trace/tcp_trace_main.c
+++ kernel_module/network/tcp_trace/tcp_trace_main.c
@@ -561,6 +561,16 @@ static int kprobe__tcp_rcv_established__pre_handler(struct kprobe *p, struct pt_
 
 
 
+static int kprobe____tcp_transmit_skb__pre_handler(struct kprobe *p, struct pt_regs *regs)
+{
+    struct sock* sk = (struct sock*)x86_64_get_regs_arg(regs, 0);
+
+    if  (!sock_filter_and_display(sk, 2, "kprobe:__tcp_transmit_skb: "))
+        return 0;
+
+    pr_debug("\n");
+    return 0;
+}
 
 
 
@@ -589,7 +599,7 @@ static struct tracepoint_probe_context sched_probes = {
 };
 
 
-#define kprobe_num 22
+#define kprobe_num 23
 
 static struct kprobe kprobes[kprobe_num] = {
     {
@@ -683,6 +693,10 @@ static struct kprobe kprobes[kprobe_num] = {
     {
         .symbol_name	= "tcp_rcv_established",
         .pre_handler = kprobe__tcp_rcv_established__pre_handler,
+    },
+    {
+        .symbol_name	= "__tcp_transmit_skb",
+        .pre_handler = kprobe____tcp_transmit_skb__pre_handler,
     },
 };
 
