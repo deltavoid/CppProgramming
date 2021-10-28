682dbb8ab51881305569dfb5e70dda5a4bd6ba14
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2021-10-28 21:57:17 +0800

    add net_seg_pton

--- kernel_module/network/net_str_parse/net_str_parse.c
+++ kernel_module/network/net_str_parse/net_str_parse.c
@@ -12,22 +12,42 @@
 
 
 const char* input = "10.16.118.56/24";
-__be32 res_net, res_prefix;
+// __be32 res_net, res_prefix;
+
+unsigned res_net, res_prefix; // host byte sequence
 
 
-
-static int net_seg_pton(const char* input, __be32* res_net_p, __be32* res_prefix_p)
+static int net_seg_pton(const char* input, unsigned* res_net_p, unsigned* res_prefix_p)
 {
-    const char* end = NULL;
-    int ret = in4_pton(input, strlen(input), (u8*)res_net_p, '/', &end);
+    __be32 res_net_net_seq, res_prefixed_net_net_seq;
+    unsigned res_net_host_seq, res_prefix_host_seq, res_prefixed_net_host_seq;
     int prefix_num = 0;
+    int ret;
+    const char* end = NULL;
 
-    pr_debug("res_net_p: %pI4\n", res_net_p);
+
+    int len = strlen(input);
+    pr_debug("len: %d\n", len);
+
+    ret = in4_pton(input, strlen(input), (u8*)&res_net_net_seq, '/', &end);
+    pr_debug("res_net_p: %pI4\n", &res_net_net_seq);
+    
+    res_net_host_seq = ntohl(res_net_net_seq);
 
 
     sscanf(end + 1, "%d", &prefix_num);
     pr_debug("prefix_num: %d\n", prefix_num);
 
+    res_prefix_host_seq = ((1 << prefix_num) - 1) << (32 - prefix_num);
+    pr_debug("prefix: %x\n", res_prefix_host_seq);
+
+    *res_net_p = res_net_host_seq;
+    *res_prefix_p = res_prefix_host_seq;
+
+    res_prefixed_net_host_seq = res_net_host_seq & res_prefix_host_seq;
+    res_prefixed_net_net_seq = htonl(res_prefixed_net_host_seq);
+
+    pr_debug("prefixed_net: %pI4\n", &res_prefixed_net_net_seq);
 }
 
 
