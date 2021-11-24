5cb1760b3cac73251357ecab00685bb3637d83c1
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2021-11-24 18:39:03 +0800

    split example to c and cpp

---
+++ c/example/hello_cmake/main.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "stdint.h"
+#include "stdbool.h"
+
+
+int main(int argc, char** argv, char** env)
+{
+    printf("env:\n");
+    for (int i = 0; env[i] != NULL; i++)
+        printf("%s\n", env[i]);
+    printf("\n");
+
+    printf("arg:\n");
+    for (int i = 0; i < argc; i++)
+        printf("%s\n", argv[i]);
+
+    return 0;
+}
