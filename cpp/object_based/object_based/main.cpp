3a3f1be42641192a48f3305ca03f40cf74d0e3bd
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2021-11-24 20:33:01 +0800

    update cpp/object_based

--- cpp/object_based/object_based/main.cpp
+++ cpp/object_based/object_based/main.cpp
@@ -1,17 +1,73 @@
 #include <cstdio>
 #include <cstdlib>
+#include <cstdint>
+#include <cstring>
+
+#include <thread>
+#include <memory>
+
+
+class Hello {
+public:
+
+    std::unique_ptr<uint32_t> _id;
+
+    Hello()
+    {
+        _id = std::make_unique<uint32_t>(0);
+    }
+
+    ~Hello()
+    {
+    }
+
+    Hello(const Hello&) = delete;
+    Hello& operator = (const Hello&) = delete;
+
+    Hello(Hello&& src)
+    {
+        this->_id = std::move(src._id);
+    }
+
+    Hello& operator = (Hello&& src)
+    {
+        if  (&src != this)
+        {
+            this->~Hello();
+
+            new (this) Hello(std::move(src));
+        }
+
+        return *this;
+    }
+
+
+    void setId(uint32_t id)
+    {
+        *this->_id = id;
+    }
+
+    uint32_t getId()
+    {
+        return *this->_id;
+    }
+
+    void printId()
+    {
+        printf("this: 0x%lx, id: %u\n", this, *this->_id);
+    }
+
+};
 
 
 int main(int argc, char** argv, char** env)
 {
-    printf("env:\n");
-    for (int i = 0; env[i] != NULL; i++)
-        printf("%s\n", env[i]);
-    printf("\n");
+    
+    Hello hello;
 
-    printf("arg:\n");
-    for (int i = 0; i < argc; i++)
-        printf("%s\n", argv[i]);
+    hello.setId(1);
+    hello.printId();
+
 
     return 0;
 }
