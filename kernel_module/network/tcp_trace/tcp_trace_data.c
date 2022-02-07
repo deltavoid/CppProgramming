0ba2aec8d8e06ceb77b8400e1394c42ac6dd670d
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2022-02-07 15:08:07 +0800

    add tcp_recvmsg in tcp_trace_data.c

--- kernel_module/network/tcp_trace/tcp_trace_data.c
+++ kernel_module/network/tcp_trace/tcp_trace_data.c
@@ -9,8 +9,47 @@
 
 
 
+// tcp_recvmsg
+
+static int kretprobe_entry__tcp_recvmsg(struct kretprobe_instance *ri, struct pt_regs *regs)
+{
+    struct sock* sk = (struct sock*)x86_64_get_regs_arg(regs, 0);
+    struct kretprobe_tcp_common_ctx* ctx = (struct kretprobe_tcp_common_ctx*)ri->data;
+    ctx->sk = NULL;
+
+    if  (!sock_filter_and_display(sk, 2, "kprobe:tcp_recvmsg"))
+        return 0;
+
+    ctx->sk = sk;
+
+    // pr_debug("\n");
+    return 0;
+}
 
 
+static int kretprobe__tcp_recvmsg(struct kretprobe_instance *ri, struct pt_regs *regs)
+{
+    struct kretprobe_tcp_common_ctx* ctx = (struct kretprobe_tcp_common_ctx*)ri->data;
+    struct sock* sk = ctx->sk;
+
+    if  (!sk)
+        return 0;
+
+    sock_common_display(sk, "kretprobe:tcp_recvmsg");
+
+    pr_debug("\n");
+    return 0;
+}
+
+const struct kretprobe kretprobe_hook__tcp_recvmsg = {
+    .kp = {
+        .symbol_name = "tcp_recvmsg",
+    },
+    .entry_handler = kretprobe_entry__tcp_recvmsg,
+    .handler = kretprobe__tcp_recvmsg,
+    .data_size = sizeof(struct kretprobe_tcp_common_ctx),
+    .maxactive = 64,
+};
 
 
 
@@ -31,11 +70,12 @@ static struct kprobe kprobes[kprobe_num] = {
 };
 
 
-#define kretprobe_num 0
+#define kretprobe_num 1
 
 static struct kretprobe kretprobes[kretprobe_num] = {
 
     // kretprobe_hook__tcp_close,
+    kretprobe_hook__tcp_recvmsg,
 
 };
 
