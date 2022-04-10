14f88fb82ff191865d85838fcce5998cd2459e9b
zqy <zqy@zqy-debian10.zqy-debian10>
2022-04-10 23:29:43 +0800

    update gflags demo

--- cpp/demo/gflags_example/main.cpp
+++ cpp/demo/gflags_example/main.cpp
@@ -1,6 +1,16 @@
 #include <cstdio>
 #include <cstdlib>
 
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+
+
+
+DEFINE_bool(echo_attachment, true, "Echo attachment as well");
+DEFINE_int32(timeout_ms, 100, "RPC timeout in milliseconds");
+DEFINE_string(server, "0.0.0.0:8000", "IP Address of server");
+
+
 
 int main(int argc, char** argv, char** env)
 {
@@ -12,6 +22,13 @@ int main(int argc, char** argv, char** env)
     printf("arg:\n");
     for (int i = 0; i < argc; i++)
         printf("%s\n", argv[i]);
+
+    // Parse gflags. We recommend you to use gflags as well.
+    google::ParseCommandLineFlags(&argc, &argv, true);
+    LOG(INFO) << "echo_attachment: " << FLAGS_echo_attachment
+            << ", timeout_ms: " << FLAGS_timeout_ms
+            << ", server: " << FLAGS_server;
+
 
     return 0;
 }
