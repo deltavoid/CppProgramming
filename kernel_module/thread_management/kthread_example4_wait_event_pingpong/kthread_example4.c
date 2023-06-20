5381a897d75c0ade2bd17d22a7e14c1e2b2f5836
zqy <850074816@qq.com>
2023-06-20 11:17:57 +0800

    update kthread_exmaple4

--- kernel_module/thread_management/kthread_example4_wait_event_pingpong/kthread_example4.c
+++ kernel_module/thread_management/kthread_example4_wait_event_pingpong/kthread_example4.c
@@ -15,16 +15,16 @@
 
 
 struct example_thread_ctx {
+    struct task_struct* task;
 
-
-    int finished;
+    int waked;
     struct wait_queue_head wq_head;
 
 };
 
 static int example_thread_ctx_init(struct example_thread_ctx* ctx)
 {
-    ctx->finished = 0;
+    ctx->waked = 0;
     init_waitqueue_head(&ctx->wq_head);
     return 0;
 }
@@ -55,32 +55,39 @@ static inline void wait_for_kthread_stop(void)
 
 static int example_thread_run(void *arg)
 {
-    int i;
+    int i = 0;
     struct example_thread_ctx* ctx = (struct example_thread_ctx*)arg;
 
     pr_debug("example_thread: 1\n");
 
 
-    for (i = 0; i < 10 && !kthread_should_stop(); i++)
+    // for (i = 0; i < 10 && !kthread_should_stop(); i++)
+    // {
+    //     pr_debug("example_thread: 2, i: %d\n", i);
+
+    //     msleep(1000);
+    // }
+
+
+    // ctx->finished = 1;
+    // wake_up(&ctx->wq_head);
+
+
+
+    // pr_debug("example_thread: 3\n");
+    // wait_for_kthread_stop();
+
+    // pr_debug("example_thread: 4\n");
+    // for (i = 0; i < 10; i++)
+    // {
+    //     pr_debug("example_thread: 4.1, i: %d\n", i);
+    //     msleep(1000);
+    // }
+
+    while (!kthread_should_stop())
     {
-        pr_debug("example_thread: 2, i: %d\n", i);
+        pr_debug("example_thread: 2, i: %d\n", ++i);
 
-        msleep(1000);
-    }
-
-
-    ctx->finished = 1;
-    wake_up(&ctx->wq_head);
-
-
-
-    pr_debug("example_thread: 3\n");
-    wait_for_kthread_stop();
-
-    pr_debug("example_thread: 4\n");
-    for (i = 0; i < 10; i++)
-    {
-        pr_debug("example_thread: 4.1, i: %d\n", i);
         msleep(1000);
     }
     
@@ -90,8 +97,86 @@ static int example_thread_run(void *arg)
 
 
 
+// ------------------------------------------------
+
+
+
+
+struct timer_example {
+
+    struct example_thread_ctx* thread_ctx;
+
+
+    struct timer_list timer;
+    uint64_t cnt;
+};
+
+#define timer_example_timeout 5
+
+static void timer_example_expire__(struct timer_example* data, struct timer_list* timer)
+{
+    pr_debug("timer_example_expire__, cnt: %lld\n", ++data->cnt);
+    // preempt_count_display();
+    // dump_stack();
+
+    // if  (++data->cnt == 10)
+    // {   pr_debug("del_timer\n");
+    //     del_timer(timer);
+    // }
+    // else
+    // {   pr_debug("jeffies: %ld\n", jiffies);
+    //     mod_timer(timer, jiffies + timer_example_timeout * HZ);
+    // }
+
+    pr_debug("jeffies: %ld\n", jiffies);
+    mod_timer(timer, jiffies + timer_example_timeout * HZ);
+    // printk("\n");
+}
+
+static void timer_example_expire(struct timer_list *timer)
+{
+    struct timer_example* data = from_timer(data, timer, timer);
+    timer_example_expire__(data, timer);
+}
+
+static int timer_example_init(struct timer_example* data)
+{
+    data->cnt = 0;
+    timer_setup(&data->timer, timer_example_expire, 0);
+    mod_timer(&data->timer, jiffies + timer_example_timeout * HZ);
+    return 0;
+}
+
+static void timer_example_exit(struct timer_example* data)
+{
+    pr_debug("timer_example_exit, timer_pending: %d\n", timer_pending(&data->timer));
+    if  (timer_pending(&data->timer))
+    {   pr_debug("timer_example_exit, del_timer\n");
+        del_timer(&data->timer);
+
+        // del_timer_sync(&data->timer); // can not be used in atomic context
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 struct task_struct* example_thread;
 struct example_thread_ctx example_thread_ctx;
+struct timer_example example_timer;
 
 
 // module init ----------------------------------------------------------
@@ -106,9 +191,18 @@ static int __init kthread_example4_init(void)
     if  (!example_thread)
         return -1;
 
+    example_thread_ctx.task = example_thread;
 
 
-    wait_event(example_thread_ctx.wq_head, example_thread_ctx.finished == 1);
+    example_timer.thread_ctx = &example_thread_ctx;
+
+
+    timer_example_init(&example_timer);
+    
+
+
+
+    // wait_event(example_thread_ctx.wq_head, example_thread_ctx.finished == 1);
 
 
     pr_info("kthread_example4_init end\n");
@@ -118,6 +212,8 @@ static int __init kthread_example4_init(void)
 static void __exit kthread_example4_exit(void)
 {
     pr_info("kthread_example4_exit begin\n");
+
+    timer_example_exit(&example_timer);
 
     kthread_stop(example_thread);
 
